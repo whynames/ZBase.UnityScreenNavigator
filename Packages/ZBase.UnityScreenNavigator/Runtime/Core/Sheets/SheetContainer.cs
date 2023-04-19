444d52ea6133f41af5109680212a7b5a82412245
Grashaar <grashaar@outlook.com>
2023-04-19 11:03:37 +0700

    Fix exception by dictionary.Add

--- Packages/ZBase.UnityScreenNavigator/Runtime/Core/Sheets/SheetContainer.cs
+++ Packages/ZBase.UnityScreenNavigator/Runtime/Core/Sheets/SheetContainer.cs
@@ -80,7 +80,7 @@ namespace ZBase.UnityScreenNavigator.Core.Sheets
         {
             _callbackReceivers.AddRange(GetComponents<ISheetContainerCallbackReceiver>());
 
-            s_instanceCacheByName.Add(_name, this);
+            s_instanceCacheByName[_name] = this;
             _canvasGroup = gameObject.GetOrAddComponent<CanvasGroup>();
         }
 
