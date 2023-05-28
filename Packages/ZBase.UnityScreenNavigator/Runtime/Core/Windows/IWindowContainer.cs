ed1d57798716f6d9db72bc0a5d469fd328dfba67
Grashaar <grashaar@outlook.com>
2023-05-28 18:17:00 +0700

    Add some APIs to IWindowContainer interface

--- Packages/ZBase.UnityScreenNavigator/Runtime/Core/Windows/IWindowContainer.cs
+++ Packages/ZBase.UnityScreenNavigator/Runtime/Core/Windows/IWindowContainer.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using ZBase.UnityScreenNavigator.Foundation.AssetLoaders;
 
 namespace ZBase.UnityScreenNavigator.Core.Windows
 {
@@ -11,5 +13,21 @@ namespace ZBase.UnityScreenNavigator.Core.Windows
         IWindowContainerManager ContainerManager { get; }
 
         Canvas Canvas { get; }
+
+        IAssetLoader AssetLoader { get; set; }
+
+        void Initialize(WindowContainerConfig config, IWindowContainerManager manager, UnityScreenNavigatorSettings settings);
+
+        bool ContainsInPool(string resourcePath);
+
+        int CountInPool(string resourcePath);
+
+        void KeepInPool(string resourcePath, int amount);
+
+        UniTask KeepInPoolAsync(string resourcePath, int amount);
+
+        void Preload(string resourcePath, bool loadAsync = true, int amount = 1);
+
+        UniTask PreloadAsync(string resourcePath, bool loadAsync = true, int amount = 1);
     }
 }
