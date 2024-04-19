6f1ae27ca79e7baae21fe1ff8474263b9dff3aa2
Grashaar <grashaar@outlook.com>
2024-04-19 16:51:00 +0700

    Support Odin Inspector

--- Packages/ZBase.UnityScreenNavigator/Editor/Core/Shared/View/ViewEditor.cs
+++ Packages/ZBase.UnityScreenNavigator/Editor/Core/Shared/View/ViewEditor.cs
@@ -4,16 +4,21 @@ using ZBase.UnityScreenNavigator.Core.Views;
 namespace ZBase.UnityScreenNavigator.Editor.Core.Shared.Views
 {
     [CustomEditor(typeof(View), editorForChildClasses: true)]
-    public class ViewEditor : UnityEditor.Editor
+    public class ViewEditor
+#if ODIN_INSPECTOR
+        : Sirenix.OdinInspector.Editor.OdinEditor
+#else
+        : UnityEditor.Editor
+#endif
     {
         public override void OnInspectorGUI()
         {
-            base.OnInspectorGUI();
-
             if (target is View view && view.DontAddCanvasGroupAutomatically == false)
             {
                 var _ = view.CanvasGroup;
             }
+
+            base.OnInspectorGUI();
         }
     }
 }
